d131bff9cf99301ec7c9c8cbbffa37f3276a987a
dangth <tranhaidang2320@gmail.com>
2019-04-16 07:25:16 +0700

    Custom parameters for feed call

--- app/src/main/java/com/dangth/foodrecipe/services/FeedAPI.java
+++ app/src/main/java/com/dangth/foodrecipe/services/FeedAPI.java
@@ -5,13 +5,14 @@ import com.dangth.foodrecipe.services.model.FeedResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Query;
 
 public interface FeedAPI {
-    @GET("/users/feed?vegetarian=false&timezone=%2B0700&size=15&from=0")
+    @GET("/users/feed?&timezone=%2B0700")
     @Headers({
             "x-auth-token: 6e8bf895-59aa-444b-b846-beec53f0e007",
             "x-tasty-auth-type: anonymous",
             "if-none-match: a0786c1cbbeb075e7080eec53ae71be8b5ab0dc8"
     })
-    Call<FeedResponse> getFeedData();
+    Call<FeedResponse> getFeedData(@Query("vegetarian") Boolean vegetarian,@Query("size") Integer size,@Query("from") Integer from);
 }
