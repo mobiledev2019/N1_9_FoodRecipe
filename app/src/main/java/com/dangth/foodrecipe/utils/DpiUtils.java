8f31edb889f03f831ad86d4c80f368d920631e9e
dangth <tranhaidang2320@gmail.com>
2019-04-21 19:01:51 +0700

    Add dpi utils

--- app/src/main/java/com/dangth/foodrecipe/utils/DpiUtils.java
+++ app/src/main/java/com/dangth/foodrecipe/utils/DpiUtils.java
@@ -1,4 +1,12 @@
 package com.dangth.foodrecipe.utils;
 
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
 public class DpiUtils {
+
+    public static int toPixels(int dp, DisplayMetrics metrics) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics);
+    }
+
 }
